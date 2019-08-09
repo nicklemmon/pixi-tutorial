66b9f7c5397e9c926b324a37d8bc79fa4e0dcf1b
Nick Lemmon <nick.lemmon@gmail.com>
2019-08-09 11:48:49 -0400

    Sprites not rendering as expected with older versions of dependencies

--- src/index.js
+++ src/index.js
@@ -2,8 +2,33 @@ import * as PIXI from 'pixi.js'
 
 const app = new PIXI.Application({
   width: 256,
-  height: 256
+  height: 256,
+  antialias: true
 })
+
+/* eslint-disable no-undef */
+function setFullScreen() {
+  app.renderer.view.style.position = 'absolute'
+  app.renderer.view.style.display = 'block'
+  app.renderer.autoResize = true
+  app.renderer.resize(window.innerWidth, window.innerHeight)
+}
+/* eslint-enable no-undef */
+
+setFullScreen()
+
+PIXI.loader
+  .add('cat.png')
+  .load(setup)
+
+// This `setup` function will run when the image has loaded
+function setup() {
+  // Create the cat sprite
+  const catSprite = PIXI.Sprite.fromImage('cat.png')
+  
+  // Add the cat to the stage
+  app.stage.addChild(catSprite)
+}
 
 /* eslint-disable no-undef */
 document.body.appendChild(app.view)
